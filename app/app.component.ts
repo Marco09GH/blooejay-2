f68ca596b6e8afda3629eb8a68ac5ef8bc9a64a4
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-05-26 00:21:49 +0200

    .

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>{{ title }}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  title: "BlooeJay 2 is coming..."
+ }
